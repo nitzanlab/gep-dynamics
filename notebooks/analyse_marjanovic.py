4b38c1dec298e30725a989cdcc7591af830b53d4
Yotam Constantini <yotamcon@cs.huji.ac.il>
2023-02-05 22:43:04 +0200

    directories setup

---
+++ notebooks/analyse_marjanovic.py
@@ -0,0 +1,24 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.4
+# ---
+
+# %% [markdown]
+# Some text
+
+# %%
+import sys
+import os
+print(sys.path)
+
+# %%
+a = 1
+
+# %%
