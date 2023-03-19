6313792742f0181b203a800170998e5343dd99cf
Yotam Constantini <yotamcon@cs.huji.ac.il>
2023-03-19 16:08:16 +0200

    pfnmf experiment

--- gepdynamics/_constants.py
+++ gepdynamics/_constants.py
@@ -13,3 +13,6 @@ NMF_TOLERANCE = 1e-6
 
 # Plotting parameters
 NON_NEG_CMAP = cubehelix_palette(start=.5, rot=-.3, light=1, as_cmap=True)
+
+# Truncated spearman correlation cuttoff 
+N_COMPARED_RANKED = 1000 
